0b1673711019690ca3deb538a0f8a4b8f98638d4
Guinueng <admin@guinueng.com>
2024-03-22 00:56:47 +0900

    Finished today's code modifying. Added open file and save result by using fopen, fgets, and fputs. Have to add binary to each type translation function and function that sums all output. And have to consider little-endian situation

--- Assignment_1/assignment1.c
+++ Assignment_1/assignment1.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "assignment1_1.h"
 
 int main(){
     FILE* input_txt = fopen("input.txt", "r");
     FILE* output_txt = fopen("output_test.txt", "a");
     char *read_line, *tot_line, *result, *reloc_mem_add;
+    char *sign_char, *ascii, *un_char, *sign_int, *un_int, *sign_float, *sign_double; // We have to save the result.
 
     if(input_txt == NULL)
         printf("input.txt does not opened normally.");
     else{
         size_t read_trials = 1;
+        size_t line = 1; // We have to consider which line we get the binaries and how many binaries in that line.
         read_line = malloc(65);
         tot_line = malloc(65);
+        // Then locate memory to get read 64 binary value in files at target line,
+        // and locate memory which have to store whole line's input.
         while( (result = fgets(read_line, 65, input_txt)) != NULL){
-            if(*read_line != '\n'){
+            // Repeat when we approach end of file.
+            if(*read_line != '\n'){ // If the case that we picked on line is not 'n'
                 if(read_trials != 1){
                     printf("Read_trials : %ld\n", read_trials);
                     reloc_mem_add = realloc(tot_line, (read_trials * 65));
-                    printf("%ld reallocated\n", (read_trials * 65));
                     if(reloc_mem_add == NULL)
                         printf("Mem reallocation failed.\n");
                     else{
@@ -32,11 +37,16 @@ int main(){
                 printf("After strcpy : \nRead line : %s \n Tot line : %s\n", read_line, tot_line);
                 read_trials++;
             }
-            else{
-                printf("Else case \\n Appeared!%s",read_line);
+            else{ // If the case that we picked on line is 'n', we can consider we approached in the final part of the line.
+                // Then we have to consider data as little-endian.
+                printf("Else\n");
+                size_t line_len = strlen(tot_line);
+                // char *sign_char, *ascii, *un_char, *sign_int, *un_int, *sign_float, *sign_double;
+                reloc_mem_add = realloc(sign_char, (line_len / 4 / sizeof(signed char)));
+                //printf("%ld", sizeof(sign_char));
+
                 printf("Tot line which appended : %s\n", tot_line);
                 fputs(tot_line, output_txt);
-                fputs("|",output_txt);
                 free(read_line);
                 free(tot_line);
                 read_line = malloc(65);
@@ -45,10 +55,9 @@ int main(){
                 printf("Reset\n\n");
             }
         }
-        printf("Else case \\n Appeared!%s",read_line);
+        printf("Else case\n");
         printf("Tot line which appended : %s\n", tot_line);
         fputs(tot_line, output_txt);
-        fputs("|",output_txt);
         free(read_line);
         free(tot_line);
         read_line = malloc(65);
