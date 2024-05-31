26bc44889757146cac7ddd705593388bc4e0e04b
Guinueng <admin@guinueng.com>
2024-06-01 04:29:45 +0900

    Modified something. Rm myfflush on myfseek and myfread based on piazza.

--- Assignment_5/main.c
+++ Assignment_5/main.c
@@ -1,8 +1,15 @@
 #include "mystdio.h"
 int main() {
-    myFILE *file = myfopen("test_flush.txt", "w");
-    myfputs("This is a test.", 15, file);
-    myfflush(file);
+    myFILE *file = myfopen("test_read_write.txt", "w+");
+    const char *text = "Sample text to write to file.";
+    myfwrite(text, sizeof(char), strlen(text), file);
+    myfseek(file, 0, SEEK_SET); // Go back to the start of the filechar buffer[BUFSIZE];
+    char buffer[BUFSIZE];
+    myfread(buffer, sizeof(char), strlen(text), file);
+    //printf("Read from file: %s\n", buffer);
+    write(1, "Read from file: ", 16);
+    write(1, buffer, 1024);
+    write(1, "\n", 1);
     myfclose(file);
     return 0;
 }
--- Assignment_5/mystdio.h
+++ Assignment_5/mystdio.h
@@ -27,6 +27,7 @@ typedef struct _myFILE{
     int mode_flag;          /* flag to describe opened mode     */
     int offset;             /* current position of a file (file position indicator) */
     int last_operation;     /* last operation for this file (read or write) */
+    int wrbuffer_len;
 } myFILE;
 
 
@@ -45,16 +46,17 @@ char* myfgets(char* str, int num, myFILE* stream);
  * ************************ */
 
 /* fopen mode: "r", "r+", "w", "w+", "a", "a+" */
+/* Read only = 0, Write only = 1, RW = 2 */
 myFILE *myfopen(const char *pathname, const char *mode){
     myFILE* tmp = malloc(sizeof(myFILE));
-    if( access(pathname, F_OK) == -1 ){
+    if(!strcmp(mode, "r")){
+        if( access(pathname, F_OK) == -1 ){ // Check File Exist.
             write(1, "File does not exist.\n", 22);
             free(tmp);
             return NULL;
         }
-    if(!strcmp(mode, "r")){
         if( access(pathname, R_OK) == -1 ){
-            write(1, "Permission Denied.\n", 20);
+            write(1, "Permission Denied\n", 19);
             free(tmp);
             return NULL;
         }
@@ -63,115 +65,93 @@ myFILE *myfopen(const char *pathname, const char *mode){
         tmp->offset = 0;
     }
     if(!strcmp(mode, "r+")){
-        if( access(pathname, R_OK) == -1  || access(pathname, W_OK) == -1){
-            write(1, "Permission Denied.\n", 20);
+        if( access(pathname, F_OK) == -1 ){
+            write(1, "File does not exist\n", 21);
             free(tmp);
             return NULL;
         }
-        tmp->fd = open(pathname, O_RDWR);
-        tmp->mode_flag = 1;
-        tmp->offset = 0;
+        if( access(pathname, R_OK) == -1  || access(pathname, W_OK) == -1){
+            write(1, "Permission Denied\n", 19);
+            free(tmp);
+            return NULL;
+        }
+        tmp -> fd = open(pathname, O_RDWR);
+        tmp -> mode_flag = 2;
+        tmp -> offset = 0;
+        tmp -> wrbuffer[0] = '\0';
+        tmp -> wrbuffer_len = 0;
     }
     if(!strcmp(mode, "w")){
-        if( access(pathname, W_OK) == -1 ){
-            write(1, "Permission Denied.\n", 20);
+        if( access(pathname, F_OK) != -1 && access(pathname, W_OK) == -1 ){
+            write(1, "Permission Denied\n", 19);
             free(tmp);
             return NULL;
         }
-        tmp->fd = open(pathname, O_WRONLY, O_TRUNC, O_CREAT);
-        tmp->mode_flag = 2;
-        tmp->offset = 0;
+        tmp -> fd = open(pathname, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        tmp -> mode_flag = 1;
+        tmp -> offset = 0;
+        tmp -> wrbuffer[0] = '\0';
+        tmp -> wrbuffer_len = 0;
     }
     if(!strcmp(mode, "w+")){
-        if( access(pathname, R_OK) == -1 || access(pathname, W_OK) == -1 ){
-            write(1, "Permission Denied.\n", 20);
+        if( access(pathname, F_OK) != -1 && (access(pathname, R_OK) == -1 || access(pathname, W_OK) == -1) ){
+            write(1, "Permission Denied\n", 19);
             free(tmp);
             return NULL;
         }
-        tmp->fd = open(pathname, O_WRONLY, O_TRUNC, O_CREAT);
-        tmp->mode_flag = 3; // Need to fix
+        tmp->fd = open(pathname, O_RDWR | O_TRUNC | O_CREAT, 0644);
+        tmp->mode_flag = 2; // Need to fix
         tmp->offset = 0;
+        tmp -> wrbuffer[0] = '\0';
+        tmp -> wrbuffer_len = 0;
     }
     if(!strcmp(mode, "a")){
-        if( access(pathname, W_OK) == -1 ){
-            write(1, "Permission Denied.\n", 20);
+        if( access(pathname, F_OK) != -1 && (access(pathname, W_OK) == -1) ){
+            write(1, "Permission Denied\n", 19);
             free(tmp);
             return NULL;
         }
-        tmp->fd = open(pathname, O_APPEND, O_CREAT);
-        tmp->mode_flag = 4;
-        tmp->offset = 0; // Need to fix
+        tmp->fd = open(pathname, O_APPEND | O_CREAT);
+        tmp->mode_flag = 1;
+        tmp->offset = 0; // Need to fix at the end of file.
+        tmp -> wrbuffer[0] = '\0';
+        tmp -> wrbuffer_len = 0;
     }
     if(!strcmp(mode, "a+")){ // Can't understand exactly.
-        if( access(pathname, R_OK) == -1 || access(pathname, W_OK) == -1 ){
-            write(1, "Permission Denied.\n", 20);
+        if( access(pathname, F_OK) != -1 && access(pathname, R_OK) == -1 || access(pathname, W_OK) == -1 ){
+            write(1, "Permission Denied\n", 19);
             free(tmp);
             return NULL;
         }
-        tmp->fd = open(pathname, O_WRONLY, O_TRUNC, O_CREAT);
-        tmp->mode_flag = 5;
-        tmp->offset = 0; // Need to fix
+        tmp->fd = open(pathname, O_WRONLY | O_TRUNC | O_CREAT);
+        tmp->mode_flag = 2;
+        tmp->offset = 0; // Need to fix value of end of file.
+        tmp -> wrbuffer[0] = '\0';
+        tmp -> wrbuffer_len = 0;
     }
     
     return tmp;
 }
 
 int myfclose(myFILE *stream){
-    if( write(stream -> fd, stream -> wrbuffer, 1024) == -1 ){ // make unwritten buffer to be written.
-        write(1, "Write failed.\n", 15);
-        return EOF;
-    }
+    myfflush(stream);
     free(stream);
     return 0;
 }
 
 int myfseek(myFILE *stream, int offset, int whence){
-    // Implement write buffer flush here.
     size_t result = lseek(stream -> fd, offset, whence);
     if(result != -1)
         return 0;
     else
         return -1;
-
-    /*size_t length;
-    while(1){
-        size_t result = read(stream -> fd, NULL, 1000);
-        if(result != 0)
-            length += result;
-        else if(result == 0)
-            break;
-    }
-    if(whence == SEEK_SET){
-        stream -> offset = 0;
-        if(0 + offset <= length){
-            stream -> offset = offset;
-            return 0;
-        }
-        else
-            return -1;
-    }
-    else if(whence == SEEK_CUR){
-        if(stream -> offset + offset <= length){
-            stream -> offset = offset;
-            return 0;
-        }
-        else
-            return -1;
-    }
-    else if(whence == SEEK_END){
-        if(length + offset <= length){
-            stream -> offset = length + offset;
-            return 0;
-        }
-        else
-            return -1;
-    }
-    else
-        return -1;*/
 }
 
 int myfread(void *ptr, int size, int nmemb, myFILE *stream){
-    // Imprement Write buffer write func.
+    if(stream -> mode_flag == 1){ // If does not have read permission.
+        write(1, "Permission Denied\n", 19);
+        return 0;
+    }
     size_t result = read(stream -> fd, ptr, size * nmemb);
     if(result > 0){
         stream -> offset += result;
@@ -179,70 +159,93 @@ int myfread(void *ptr, int size, int nmemb, myFILE *stream){
     }
     else
         return 0;
-    
-    /*
-    int r_qty = 0;
-    if((stream -> offset % 1024) == 0)
-        read(stream -> fd, stream -> rdbuffer, 1024);
-    if( (stream -> offset % 1024) + size * nmemb < 1024 ){
-        for(size_t i = 0; i < size * nmemb; i++)
-            *(ptr + i) = stream -> rdbuffer[(stream -> offset % 1024) + i];
-    }
-    else{
-        size_t readable_len = 1024 - (stream -> offset % 1024);
-        for(size_t i = 0; i < readable_len; i++)
-            ptr + i = stream -> rdbuffer[(stream -> offset % 1024) + size * i];
-        stream -> offset += readable_len;
-        size_t read_len = read(stream -> fd, stream -> rdbuffer, 1024);
-        size_t must_read_len = size * nmemb - readable_len;
-        if(must_read_len < read_len)
-            for(size_t i = 0; i < must_read_len; i++)
-                ptr + readable_len + i = stream -> rdbuffer[i];
-        else
-            for(size_t i = 0; i < read_len; i++)
-                ptr + readable_len + i = stream -> rdbuffer[i];
-    }*/
 }
 
 int myfwrite(const void *ptr, int size, int nmemb, myFILE *stream){ // Need to add buffer.
-    
+    if(stream -> mode_flag == 0){ // If does not have write permission.
+        write(1, "Permission Denied\n", 19);
+        return 0;
+    }
     const char* char_ptr = ptr;
     size_t tot_size = size * nmemb;
-    size_t buf_len = strlen(stream -> wrbuffer);
+    size_t buf_len = stream -> wrbuffer_len;
     size_t tot_trial = (tot_size + buf_len) / 1024;
     size_t cur_loc = 0;
+    size_t result = 0;
     if((tot_size + buf_len) % 1024 != 0)
         tot_trial++;
-    for(size_t i = buf_len; i < 1024; i++)
-        stream -> wrbuffer[i] = char_ptr[cur_loc++];
-    size_t result = write(stream -> fd, stream -> wrbuffer, size * nmemb);
+    
+    if(tot_size >= 1024){
+        for(size_t i = buf_len; i < 1024; i++)
+            stream -> wrbuffer[i] = char_ptr[cur_loc++];
+        result = write(stream -> fd, stream -> wrbuffer, size * nmemb);
+    }
+    else
+        for(size_t i = buf_len; i < buf_len + tot_size; i++)
+            stream -> wrbuffer[stream -> wrbuffer_len++] = char_ptr[cur_loc++];
+    
     for(size_t i = 1; i < tot_trial; i++){
-        if(i != tot_trial)
+        stream -> wrbuffer_len = (tot_size + buf_len) % 1024;
+        if(stream -> wrbuffer_len == 0)
+            stream -> wrbuffer_len = 1024;
+        for(size_t j = 0; j < stream -> wrbuffer_len; j++)
+            stream -> wrbuffer[j] = char_ptr[cur_loc++];
+        if(stream -> wrbuffer_len == 1024){
+            result = write(stream -> fd, stream -> wrbuffer, 1024);
+            stream -> wrbuffer_len = 0;    
+        }
+        /*
+        if(i != tot_trial){
             for(size_t j = 0; j < 1024; j++)
                 stream -> wrbuffer[j] = char_ptr[cur_loc++];
-        else{
-            for(size_t j = 0; j < (tot_size + buf_len) % 1024; j++)
-                stream -> wrbuffer[j] = char_ptr[cur_loc++];
+            result = write(stream -> fd, stream -> wrbuffer, 1024);
         }
-        result = write(stream -> fd, stream -> wrbuffer, 1024);
+        else{
+            stream -> wrbuffer_len = (tot_size + buf_len) % 1024;
+            if(stream -> wrbuffer_len == 0)
+                stream -> wrbuffer_len = 1024;
+            for(size_t j = 0; j < stream -> wrbuffer_len; j++)
+                stream -> wrbuffer[j] = char_ptr[cur_loc++];
+            if(stream -> wrbuffer_len == 1024){
+                result = write(stream -> fd, stream -> wrbuffer, 1024);
+                stream -> wrbuffer_len = 0;    
+            }
+        }*/
+        
     }
-    if(result > 0)
+    if(result > 0 || stream -> wrbuffer_len > 0)
         return result;
     else
         return 0;
 }
 
-int myfflush(myFILE *stream){
-    if(write(stream -> fd, stream -> wrbuffer, 1024) >= 0)
+int myfflush(myFILE *stream){ // Let set file offset in the final part of file. Then write it. And return to original offset.
+    if(stream -> mode_flag == 0){ // If it has read only permission.
+        return 0; // Nothing to do. Read buffer is dismissed.(Based on piazza comments.)
+    }
+    if(stream -> wrbuffer_len == 0)
+        return 0;
+    else if( stream -> wrbuffer_len > 0 && write(stream -> fd, stream -> wrbuffer, stream -> wrbuffer_len) >= 0)
         return 0;
     else
         return EOF;
 }
 
-int myfputs(const char* str, int num, myFILE* stream){ // Need to add buffer.
-    size_t result = write(stream -> fd, str, num);
+int myfputs(const char* str, int num, myFILE* stream){
+    if(stream -> mode_flag == 0){ // If does not have write permission.
+        write(1, "Permission Denied\n", 19);
+        return 0;
+    }
+    // Need to add buffer.
+    size_t len = 0;
+    if(strlen(str) < num)
+        len = strlen(str);
+    else
+        len = num;
+    size_t result = write(stream -> fd, str, len);
     if(result > 0){
         write(stream -> fd, "\0", 1);
+        stream -> wrbuffer_len = 0;
         return 0;
     }
     else
@@ -250,6 +253,10 @@ int myfputs(const char* str, int num, myFILE* stream){ // Need to add buffer.
 }
 
 char* myfgets(char *str, int num, myFILE* stream){
+    if(stream -> mode_flag == 1){ // If does not have read permission.
+        write(1, "Permission Denied\n", 19);
+        return NULL;
+    }
     char tmp;
     for(size_t i = 0; i < num - 1; i++){
         if(read(stream -> fd, &tmp, 1) != 0){
